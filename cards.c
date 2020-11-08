bb5549af5cf9e605312092633b0398cfe3d6227b
Jury <1jura.vas@gmail.com>
2020-11-08 18:36:23 +0300

    First commit

---
+++ cards.c
@@ -0,0 +1,32 @@
+/*
+ * Программа для оценивания важности карт.
+ * Выпущенопод публичной Лас-Вегасской лицензией.
+ * (с) 2020 Команда колледжа по блек-джеку.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	char card_name[3];
+	puts("Введите название карты: ");
+	scanf("%2s", card_name);
+	int val = 0;
+	switch(card_name[0])  {
+		case 'K':
+			val = 10;
+			break;
+		case 'Q':
+			val = 10;
+			break;
+		case 'J':
+			val = 10;
+			break;
+		default:
+			val = atoi(card_name);
+	}
+	printf("Ценность карты: %i\n", val);
+	return 0;
+}
+
